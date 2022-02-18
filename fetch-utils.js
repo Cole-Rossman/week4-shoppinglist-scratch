30e061da920d0ddeef2dc27612c0c1ea90276e78
Cole Rossman <colerossman99@gmail.com>
2022-02-17 16:11:16 -0800

    getItems function added to render-utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,16 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://oulietmefshvjsdknixn.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91bGlldG1lZnNodmpzZGtuaXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ0NTAyNzcsImV4cCI6MTk2MDAyNjI3N30.xLaRALCsmulIZooLc5FTZDT0SLfAZjEKegSSVtKRgFI';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
+export async function getShoppingList() {
+    const resp = await client.from('items').select('*').order('id');
+
+    return checkError(resp);
+}
+
+
 
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
@@ -37,6 +46,6 @@ export async function logout() {
     return (window.location.href = '../');
 }
 
-// function checkError({ data, error }) {
-//     return error ? console.error(error) : data;
-// }
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
